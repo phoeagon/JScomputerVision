1b29096a7e0e2966c96956c4b0daf5f51af78c34
Qiu Zhe <phoeagon@gmail.com>
2013-09-27 20:20:00 +0800

    more routines

--- js/cv.js
+++ js/cv.js
@@ -9,6 +9,13 @@
 //-----------------------------
 //
 
+// ## Global Color Object
+//
+colors = {
+	white : [255 , 255 , 255 ] ,
+	black : [ 0 , 0 ,  0 ] 
+};
+
 // The `CV` object
 //----------------
 // The main CV object that stores all sorts of methods
@@ -20,6 +27,8 @@ CV = {
 // This constructor takes in the image data object as its parameter 
 // and construct a corresponding `CV` object in which the image is stored.
 //
+// This routine returns the `CV` object constructed.
+//
 function CV ( imgData ){
 	this.imgData = imgData;
 }
@@ -28,12 +37,25 @@ function CV ( imgData ){
 // 
 // Generate histagram information of a grayscale image.
 //
+// This routine returns the histagram as an `Array`
+//
 CV.prototype.histogram = function( ){
-	var hist = [];
-	var i;//placeholder
+	var hist = [] , len = this.imgData.length ;
+	var imgData = this.imgData;
+	var i;/*placeholder*/
 	for ( i=0; i<256; ++i )
 		hist[i] = 0;
-	
+	for ( i=0; i < len; ++i ){
+		if ( i % 4 != 3 ){// skip the alpha channel
+			hist[ imgData[i] ]++;
+		}
+	}
+	var sum = 0;
+	for ( i = 0 ; i < 256 ; ++i )
+		sum += hist[i];
+	for ( int i = 0 ; i < 256 ; ++ i )
+		hist[i] /= sum;
+	return hist;
 }
 
 // ## `getImgData`  
@@ -50,6 +72,8 @@ CV.prototype.getImgData = function (){
 // arithmetic operation) or instead an `Array` like `[1/3,1/6,1/2]`, whose
 // three elements should sum to zero (otherwise a default *even-weight* 
 // strategy is used as fallback
+//
+// This routine returns the `CV` object with modified image data
 //
 CV.prototype.grayscale = function ( weight ){
 	try{ /* put into try catch just in case some typeError occurs LOL*/
@@ -73,6 +97,7 @@ CV.prototype.grayscale = function ( weight ){
 		for ( j = 0 ; j < 3 ; ++ j )
 			imgData[j] = sum ;
 	}
+	return this;
 }
 
 // ##`otsu`
@@ -81,5 +106,38 @@ CV.prototype.grayscale = function ( weight ){
 //	algorithm to determine a threshold with which
 // to threshold.
 //
+// This routine returns the threshold value
+//
 CV.prototype.otsu = function( ){
+	return 0;
+}
+
+// ## `thresholding`
+//
+// This routine 
+CV.prototype.threshold = function( thres , white , black ){
+	if ( thres == null )
+		thres = this.otsu();	//auto threshold
+	if ( white == null || black == null ){
+		white = colors.white ;
+		black = colors.black ;
+	}
+	/* we assume that the image is already in grayscale here. */
+	var i , j ,  len = this.imgData.length , img = this.imgData ;
+	for ( i = 0; i < len ; i += 4 ){
+		var sum = 0;
+		for ( j = 0 ; j < 3 ; ++ j )
+			sum += imgData[i+j] ; 
+		sum = Math.round( sum / 3 );
+		if ( sum >= thres )
+		{ /* set to white */
+			for ( j = 0; j<3; ++j )
+				imgData[ i+j ] = white[ j ] ;
+		}else{
+			/* set to black*/
+			for ( j = 0 ; j < 3 ; ++j )
+				imgData[ i+j ] = black[ j ] ;
+		}
+	}
+	return this;
 }
