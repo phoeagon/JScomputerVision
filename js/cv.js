c7d0399e26b316fd58859b4d878577f35247e40f
Qiu Zhe <phoeagon@gmail.com>
2013-09-27 19:41:39 +0800

    added cv.js

---
+++ js/cv.js
@@ -0,0 +1,28 @@
+//
+//	CV Lib
+//============================
+//		by Qiu Zhe ( phoeagon_AT_gmail.com )
+//
+//	This library is implemented as part of a course project for 
+//  Computer Vision (2013 Fall).
+//
+//-----------------------------
+//
+// The main CV object that stores all sorts of methods
+CV = {
+};
+// #`cv.histagram` - Histagram
+// 
+// Generate histagram information of a grayscale image.
+//
+CV.histogram = function( source ){
+	var hist = [];
+	var i;//placeholder
+	for ( i=0; i<256; ++i )
+		hist[i] = 0;
+	
+}
+CV.grayscale = function ( source ){
+}
+CV.otsu = function( source ){
+}
